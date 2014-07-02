4305325f1a1f315398f71a970eaa25b8670a3715
Grervirg <Grervirg@gmail.com>
2014-07-01 21:34:41 -0400

    Moved to Proper Repository

--- Scripts/ProcessMovies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-Inputpath=/media/Downloads/Process/Movies
-Outputpath=/media/MyMedia/Movies/
-Format="{n} ({y})[{sdhd}]/{n} ({y})"
-filebot -rename /media/Downloads/Process/Movies --db IMDB --format "{n} ({y})[{sdhd}]/{n} ({y})" --output /media/MyMedia/Movies/ 
--- Scripts/ProcessTv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-Inputpath=/media/Downloads/"Process Tv"/
-Outputpath=/media/MyMedia/"Tv Shows"/
-Format="{n}/Season {s}/{n} {sxe} {t}"
-Filebot -rename $Inputpath --db thetvdb --format $Format --output $Outputpath 
